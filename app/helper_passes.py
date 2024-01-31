4b09bc8d3a338e1aff63a7723b3494eb186653ff
Petro Ivaniuk <Ivanyukpetro@gmail.com>
2024-01-31 03:44:08 +0200

    update passes tab (#15)

--- app/helper_passes.py
+++ app/helper_passes.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.cluster import KMeans
 
 import seaborn as sns
-from sklearn.cluster import KMeans
-from matplotlib.patches import Arc
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
 from matplotlib.legend_handler import HandlerLine2D
 from matplotlib.patches import FancyArrowPatch
 from mplsoccer.pitch import VerticalPitch
+from mplsoccer import Pitch
 from scipy.spatial import ConvexHull, Voronoi, Delaunay
 
 
@@ -178,7 +178,7 @@ def plot_pass_map(df_player_pass, df_location, title, sub_title, metric, color, 
     ARROW_SHIFT = 2
     cmap = plt.cm.get_cmap(cmap_name)
 
-    fig, ax = VerticalPitch().draw(nrows=1, ncols=1, figsize=(16, 10))
+    fig, ax = VerticalPitch().draw(nrows=1, ncols=1, figsize=(24, 16))
     fig.set_facecolor("white")
 
     if not df_player_pass.empty:
@@ -250,7 +250,7 @@ def plot_pass_map(df_player_pass, df_location, title, sub_title, metric, color, 
         va='top')
     ax.text(0, 124, 
         sub_title,
-        fontsize=12, 
+        fontsize=18, 
         va='top')
 
     title.set_path_effects([
@@ -258,14 +258,16 @@ def plot_pass_map(df_player_pass, df_location, title, sub_title, metric, color, 
         pe.PathPatchEffect(edgecolor='black', linewidth=.8, facecolor=color)
     ])
 
-    if not df_player_pass.empty:
-        # add legend for annotations
-        h, _ = ax.get_legend_handles_labels()
-        annotate = annotations[-1]
+    # if not df_player_pass.empty:
+    # add legend for annotations
+    h, _ = ax.get_legend_handles_labels()
+    annotate = annotations[-1]
+#     print(type(annotate), annotate)
 
-        ax.legend(handles=h + [annotate],
-                  handler_map={type(annotate): AnnotationHandler(5)},
-                  loc=3, bbox_to_anchor=(0, -0.05))
+    ax.legend(handles = h + [annotate],
+              handler_map={type(annotate) : AnnotationHandler(5)},
+              loc=3)
+
     st.pyplot(fig, use_container_width=True)
 
 
@@ -275,158 +277,6 @@ def plot_pass_heatmap(df):
     st.pyplot(fig, use_container_width=True)
 
 
-def draw_pitch(x_min=0,
-               x_max=106,
-               y_min=0,
-               y_max=68,
-               pitch_color="w",
-               line_color="grey",
-               line_thickness=1.5,
-               point_size=20,
-               orientation="horizontal",
-               aspect="full",
-               ax=None
-               ):
-    if orientation.lower().startswith("h"):
-        first = 0
-        second = 1
-        arc_angle = 0
-
-        if aspect == "half":
-            ax.set_xlim(x_max / 2, x_max + 5)
-
-    elif orientation.lower().startswith("v"):
-        first = 1
-        second = 0
-        arc_angle = 90
-
-        if aspect == "half":
-            ax.set_ylim(x_max / 2, x_max + 5)
-
-
-    else:
-        raise NameError("You must choose one of horizontal or vertical")
-
-    ax.axis("off")
-
-    rect = plt.Rectangle((x_min, y_min),
-                         x_max, y_max,
-                         facecolor=pitch_color,
-                         edgecolor="none",
-                         zorder=-2)
-
-    ax.add_artist(rect)
-
-    x_conversion = x_max / 100
-    y_conversion = y_max / 100
-
-    pitch_x = [0, 5.8, 11.5, 17, 50, 83, 88.5, 94.2, 100]  # pitch x markings
-    pitch_x = [x * x_conversion for x in pitch_x]
-
-    pitch_y = [0, 21.1, 36.6, 50, 63.2, 78.9, 100]  # pitch y markings
-    pitch_y = [x * y_conversion for x in pitch_y]
-
-    goal_y = [45.2, 54.8]  # goal posts
-    goal_y = [x * y_conversion for x in goal_y]
-
-    # side and goal lines
-    lx1 = [x_min, x_max, x_max, x_min, x_min]
-    ly1 = [y_min, y_min, y_max, y_max, y_min]
-
-    # outer boxed
-    lx2 = [x_max, pitch_x[5], pitch_x[5], x_max]
-    ly2 = [pitch_y[1], pitch_y[1], pitch_y[5], pitch_y[5]]
-
-    lx3 = [0, pitch_x[3], pitch_x[3], 0]
-    ly3 = [pitch_y[1], pitch_y[1], pitch_y[5], pitch_y[5]]
-
-    # goals
-    lx4 = [x_max, x_max + 2, x_max + 2, x_max]
-    ly4 = [goal_y[0], goal_y[0], goal_y[1], goal_y[1]]
-
-    lx5 = [0, -2, -2, 0]
-    ly5 = [goal_y[0], goal_y[0], goal_y[1], goal_y[1]]
-
-    # 6 yard boxes
-    lx6 = [x_max, pitch_x[7], pitch_x[7], x_max]
-    ly6 = [pitch_y[2], pitch_y[2], pitch_y[4], pitch_y[4]]
-
-    lx7 = [0, pitch_x[1], pitch_x[1], 0]
-    ly7 = [pitch_y[2], pitch_y[2], pitch_y[4], pitch_y[4]]
-
-    # Halfway line, penalty spots, and kickoff spot
-    lx8 = [pitch_x[4], pitch_x[4]]
-    ly8 = [0, y_max]
-
-    lines = [
-        [lx1, ly1],
-        [lx2, ly2],
-        [lx3, ly3],
-        [lx4, ly4],
-        [lx5, ly5],
-        [lx6, ly6],
-        [lx7, ly7],
-        [lx8, ly8],
-    ]
-
-    points = [
-        [pitch_x[6], pitch_y[3]],
-        [pitch_x[2], pitch_y[3]],
-        [pitch_x[4], pitch_y[3]]
-    ]
-
-    circle_points = [pitch_x[4], pitch_y[3]]
-    arc_points1 = [pitch_x[6], pitch_y[3]]
-    arc_points2 = [pitch_x[2], pitch_y[3]]
-
-    for line in lines:
-        ax.plot(line[first], line[second],
-                color=line_color,
-                lw=line_thickness,
-                zorder=-1)
-
-    for point in points:
-        ax.scatter(point[first], point[second],
-                   color=line_color,
-                   s=point_size,
-                   zorder=-1)
-
-    circle = plt.Circle((circle_points[first], circle_points[second]),
-                        x_max * 0.088,
-                        lw=line_thickness,
-                        color=line_color,
-                        fill=False,
-                        zorder=-1)
-
-    ax.add_artist(circle)
-
-    arc1 = Arc((arc_points1[first], arc_points1[second]),
-               height=x_max * 0.088 * 2,
-               width=x_max * 0.088 * 2,
-               angle=arc_angle,
-               theta1=128.75,
-               theta2=231.25,
-               color=line_color,
-               lw=line_thickness,
-               zorder=-1)
-
-    ax.add_artist(arc1)
-
-    arc2 = Arc((arc_points2[first], arc_points2[second]),
-               height=x_max * 0.088 * 2,
-               width=x_max * 0.088 * 2,
-               angle=arc_angle,
-               theta1=308.75,
-               theta2=51.25,
-               color=line_color,
-               lw=line_thickness,
-               zorder=-1)
-
-    ax.add_artist(arc2)
-    ax.set_aspect("equal")
-    return ax
-
-
 def mode_including_nan(series):
     series_filled = series.fillna('NaN')
     mode_value = series_filled.mode()[0]
@@ -434,35 +284,18 @@ def mode_including_nan(series):
         return np.nan
     return mode_value
 
-
-def create_pass_heatmap(data, team, n_clusters=40):
-    pass_data = data[(data['type'] == "Pass") & (data['team'] == team)].reset_index(drop=True)
-
+def create_pass_heatmap(pass_data, n_clusters=40):
+    """
+    """
+    color_background = '#F7F7F7'
     pass_data['location_x'] = pass_data['location'].apply(lambda x: x[0])
     pass_data['location_y'] = pass_data['location'].apply(lambda x: x[1])
     pass_data['pass_end_location_x'] = pass_data['pass_end_location'].apply(lambda x: x[0])
     pass_data['pass_end_location_y'] = pass_data['pass_end_location'].apply(lambda x: x[1])
 
-    background = '#F7F7F7'
-    plt.rcParams.update(plt.rcParamsDefault)
-    plt.rcParams['xtick.color'] = 'black'
-    plt.rcParams['ytick.color'] = 'black'
-    plt.rcParams.update({'font.size': 18})
-
-    fig, ax = plt.subplots(figsize=(16.5, 10.5))
-    fig.set_facecolor(background)
-    ax.patch.set_facecolor(background)
-
-    pitch = draw_pitch(x_min=0,
-                       x_max=120,
-                       y_min=0,
-                       y_max=80,
-                       orientation='horizontal',
-                       aspect='fll',
-                       pitch_color='#F7F7F7',
-                       line_color='#3B3B3B',
-                       ax=ax
-                       )
+    pitch = Pitch(pitch_type='statsbomb', pitch_color=color_background, line_color='#3B3B3B')
+    fig, ax = pitch.draw(figsize=(16, 11), constrained_layout=True, tight_layout=False)
+    fig.set_facecolor("white")
 
     kde = sns.kdeplot(pass_data,
                       x='location_x',
@@ -503,45 +336,33 @@ def create_pass_heatmap(data, team, n_clusters=40):
     plt.xlim(0 - 5, 125)
     plt.ylim(0 - 5, 85)
 
-    plt.figtext(
-        0.055,
-        0.96,
-        f'{team} pass heatmap',
-        fontsize=25,
-        fontweight='bold',
-        color='black'
-    )
-    plt.tight_layout()
     st.pyplot(fig)
 
 
-def plot_hulls(team_dataset, lineups, team, main_color):
+def plot_hulls(team_dataset, lineups, main_color):
+    """
+    """
     unique_players = team_dataset['player'].unique()
     n_rows = (len(unique_players) + 3) // 4
 
-    fig, axs = plt.subplots(n_rows, 4, figsize=(20, 18))
+    fig, axs = plt.subplots(n_rows, 4, figsize=(16, 24), dpi=150)
     axs = axs.flatten()
 
     for idx, player in enumerate(unique_players):
-        if type(player) != str:
-            continue
         pitch = VerticalPitch(pitch_type='statsbomb', pitch_color=None, corner_arcs=True)
         pitch.draw(axs[idx])
 
         xmin, xmax, ymin, ymax = pitch.extent
         axs[idx].xaxis.set_ticks([xmin, xmax])
         axs[idx].yaxis.set_ticks([ymin, ymax])
-        axs[idx].tick_params(labelsize=15)
-        axs[idx].set_title(player, fontsize=18, pad=15)
+        axs[idx].tick_params(labelsize=10)
+        axs[idx].set_title(player, fontsize=15, pad=10)
 
         pos = list(lineups[lineups['player_name'] == player]['positions'].to_dict().values())
-        if len(pos):
-            pos = pos[0][0]['position']
-        else:
-            pos = ''
+        pos = pos[0][0]['position'] if len(pos) else ''
         axs[idx].text(85, 58.0, pos, size=12, verticalalignment='center', rotation=270)
 
-        player_data = team_dataset[(team_dataset['player'] == player) & (team_dataset['type'] == 'Pass')]
+        player_data = team_dataset[(team_dataset['player'] == player)]
         player_data['x'] = player_data['location'].apply(lambda x: x[0])
         player_data['y'] = player_data['location'].apply(lambda x: x[1])
 
@@ -562,9 +383,8 @@ def plot_hulls(team_dataset, lineups, team, main_color):
                 axs[idx].plot(pos_xy[i, 1], pos_xy[i, 0], 'black')
                 axs[idx].fill(pos_xy[ch.vertices, 1], pos_xy[ch.vertices, 0], alpha=0.1)
 
-    fig.suptitle(f"{team} players Convex Hulls", fontsize=25, verticalalignment='top', y=1.01)
-    plt.tight_layout()
-    axs[0].set_axis_off()
+    # TODO: do next lines without hard coding
+    axs[-3].set_axis_off()
     axs[-2].set_axis_off()
     axs[-1].set_axis_off()
     st.pyplot(fig)
